ad5d85b0f240f4fae26b4df7b4d3ababa895856e
Your Name <your-email@example.com>
2026-01-19 03:18:58 +0200

    chore(seo): refine next-sitemap configuration for preview-post.com

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,9 +2,6 @@
 module.exports = {
   siteUrl: 'https://preview-post.com',
   generateRobotsTxt: true,
-  robotsTxtOptions: {
-    additionalSitemaps: [
-      'https://preview-post.com/sitemap.xml',
-    ],
-  },
+  exclude: ['/sitemap.xml', '/robots.txt'],
 }
+
